9b4a287bb032affecce74980913c7fc22bbcce5c
NP-jaehyoung <dkswogud12@kangwon.ac.kr>
2026-02-15 22:35:10 +0900

    Remove obsolete root copies after 20-story folder migration

--- solveConstrainedStatic.m
+++
@@ -1,6 +0,0 @@
-function displacements = solveConstrainedStatic(GDof, prescribedDof, stiffness, force)
-activeDof = setdiff((1:GDof)', [prescribedDof]);
-U = stiffness(activeDof,activeDof)\force(activeDof);
-displacements = zeros(GDof,1);
-displacements(activeDof) = U;
-end
