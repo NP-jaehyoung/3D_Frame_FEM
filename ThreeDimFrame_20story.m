79d8cd68523ab66721496b0b5126b1f37ece5a16
NP-jaehyoung <dkswogud12@kangwon.ac.kr>
2026-02-15 20:11:08 +0900

    Add 20-story 3D frame FEM example script

---
+++ ThreeDimFrame_20story.m
@@ -0,0 +1,242 @@
+%................................................................
+% MATLAB codes for Finite Element Analysis
+% 20-story 3D frame example
+% clear memory
+clear all
+clc
+
+%% model settings
+numFloors = 20;            % number of floor levels
+floorHeight = 4.0;         % story height [m]
+spanX = 4.0;               % x direction span [m]
+spanZ = 4.0;               % z direction span [m]
+E = 2e7;                   % modulus of elasticity [kPa]
+rho = 7850;                % density
+width = 1;                 
+depth = 1;                 
+A = width*depth;           % area [m^2]
+Iy = 1/12;
+Iz = 1/12;
+nu = 0.167;
+J = 2.25*1^4;             
+G = E/(2*(1+nu));
+Ptop = -3000;              % single lateral load at top story [kN]
+
+%% node coordinates: first two floors
+nodeCoordinates = [...
+    0       0          0;  % 1
+    0       0        spanZ; % 2
+    spanX  0        spanZ; % 3
+    spanX  0          0;  % 4
+    0       floorHeight  0; % 5
+    0       floorHeight spanZ; % 6
+    spanX  floorHeight spanZ; % 7
+    spanX  floorHeight  0; % 8
+];
+for i = 3:numFloors
+    baseY = floorHeight*(i-1);
+    nodeCoordinates(4*(i-1)+1,:) = [0 baseY 0];
+    nodeCoordinates(4*(i-1)+2,:) = [0 baseY spanZ];
+    nodeCoordinates(4*(i-1)+3,:) = [spanX baseY spanZ];
+    nodeCoordinates(4*(i-1)+4,:) = [spanX baseY 0];
+end
+
+%% element connectivity: columns + girders each floor
+elementNodes = [1 5; 2 6; 3 7; 4 8; 5 6; 6 7; 7 8; 8 5];
+for i = 3:numFloors
+    base = 4*(i-2);
+    elementNodes(8*(i-2)+1,:) = [base+1 base+5];
+    elementNodes(8*(i-2)+2,:) = [base+2 base+6];
+    elementNodes(8*(i-2)+3,:) = [base+3 base+7];
+    elementNodes(8*(i-2)+4,:) = [base+4 base+8];
+    elementNodes(8*(i-2)+5,:) = [base+5 base+6];
+    elementNodes(8*(i-2)+6,:) = [base+6 base+7];
+    elementNodes(8*(i-2)+7,:) = [base+7 base+8];
+    elementNodes(8*(i-2)+8,:) = [base+8 base+5];
+end
+
+numberNodes = size(nodeCoordinates,1);
+numberElements = size(elementNodes,1);
+GDof = 6*numberNodes;
+
+%% assemble K, M
+force = zeros(GDof,1);
+stiffness = formStiffness3Dframe(GDof, numberElements, elementNodes,...
+    numberNodes, nodeCoordinates, E, A, Iz, Iy, G, J);
+mass = formMass3Dframe(GDof, numberElements, elementNodes,...
+    numberNodes, nodeCoordinates, rho, A, Iz, Iy);
+
+%% boundary conditions: fix 1st floor nodes (nodes 1~4)
+prescribedDof = 1:24;
+
+%% top story one-point load at DOF UX of node 3 of top floor
+topNode = 4*(numFloors-1) + 3;   % 1st corner node on top floor
+topLoadDof = 6*topNode - 5;      % UX at that node
+force(topLoadDof) = Ptop;
+
+%% static solution and output
+displacements = solution(GDof,prescribedDof,stiffness,force);
+reactions = stiffness*displacements - force;
+reactionSupport = reactions(prescribedDof);
+
+fprintf("20-floor 3D frame example\n");
+fprintf("Top node: %d, top load DOF: %d, load: %.4g\n", topNode, topLoadDof, force(topLoadDof));
+fprintf("Top node displacement (UX,UY,UZ) [m]\n");
+fprintf("UX = %.6e\n", displacements(topLoadDof));
+fprintf("UY = %.6e\n", displacements(topLoadDof+1));
+fprintf("UZ = %.6e\n\n", displacements(topLoadDof+2));
+
+fprintf("Support reactions (first 24 DOF)\n");
+for ii = 1:numel(prescribedDof)
+    fprintf("DOF %d : %+13.6e\n", prescribedDof(ii), reactionSupport(ii));
+end
+
+%% modal analysis
+maxModesToShow = 10;
+[modes,eigenvalues] = eigenvalue(GDof,prescribedDof,stiffness,mass,maxModesToShow);
+omega = sqrt(abs(eigenvalues));           % rad/s
+freqHz = omega/(2*pi);                   % Hz
+[freqHzSorted, sortIdx] = sort(freqHz,"ascend");
+fprintf("\nNatural frequencies (Hz)\n");
+for iMode = 1:min(maxModesToShow,length(freqHzSorted))
+    fprintf("Mode %2d : %12.8e\n", iMode, freqHzSorted(iMode));
+end
+
+% optional access to mode shapes:
+_ = modes;
+
+%% local functions
+function  [stiffness]=formStiffness3Dframe(GDof,numberElements,elementNodes,numberNodes,nodeCoordinates,E,A,Iz,Iy,G,J);
+stiffness = zeros(GDof);
+for e = 1:numberElements
+    indice = elementNodes(e,:);
+    elementDof=[6*indice(1)-5 6*indice(1)-4 6*indice(1)-3 ...
+        6*indice(1)-2 6*indice(1)-1 6*indice(1)...
+        6*indice(2)-5 6*indice(2)-4 6*indice(2)-3 ...
+        6*indice(2)-2 6*indice(2)-1 6*indice(2)] ;
+    x1=nodeCoordinates(indice(1),1);
+    y1=nodeCoordinates(indice(1),2);
+    z1=nodeCoordinates(indice(1),3);
+    x2=nodeCoordinates(indice(2),1);
+    y2=nodeCoordinates(indice(2),2);
+    z2=nodeCoordinates(indice(2),3);
+    L = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) +...
+        (z2-z1)*(z2-z1));
+    k1 = E*A/L;
+    k2 = 12*E*Iz/(L*L*L);
+    k3 = 6*E*Iz/(L*L);
+    k4 = 4*E*Iz/L;
+    k5 = 2*E*Iz/L;
+    k6 = 12*E*Iy/(L*L*L);
+    k7 = 6*E*Iy/(L*L);
+    k8 = 4*E*Iy/L;
+    k9 = 2*E*Iy/L;
+    k10 = G*J/L;
+    a=[k1 0 0; 0 k2 0; 0 0 k6];
+    b=[ 0 0 0;0 0 k3; 0 -k7 0];
+    c=[k10 0 0;0 k8 0; 0 0 k4];
+    d=[-k10 0 0;0 k9 0;0 0 k5];
+    k = [a b -a b;b' c b d; (-a)' b' a -b;b' d' (-b)' c];
+    if x1 == x2 && y1 == y2
+        if z2 > z1
+            Lambda = [0 0 1 ; 0 1 0 ; -1 0 0];
+        else
+            Lambda = [0 0 -1 ; 0 1 0 ; 1 0 0];
+        end
+    else
+        CXx = (x2-x1)/L;
+        CYx = (y2-y1)/L;
+        CZx = (z2-z1)/L;
+        D = sqrt(CXx*CXx + CYx*CYx);
+        CXy = -CYx/D;
+        CYy = CXx/D;
+        CZy = 0;
+        CXz = -CXx*CZx/D;
+        CYz = -CYx*CZx/D;
+        CZz = D;
+        Lambda = [CXx CYx CZx ;CXy CYy CZy ;CXz CYz CZz];
+    end
+    R = [Lambda zeros(3,9); zeros(3) Lambda zeros(3,6);
+        zeros(3,6) Lambda zeros(3);zeros(3,9) Lambda];
+    stiffness(elementDof,elementDof)=...
+        stiffness(elementDof,elementDof)+R'*k*R;
+end
+end
+
+function  [mass] = formMass3Dframe(GDof,numberElements, ...
+    elementNodes,numberNodes,nodeCoordinates,rho,A,Iz,Iy)
+mass = zeros(GDof);
+for e = 1:numberElements
+    indice = elementNodes(e,:);
+    elementDof=[6*indice(1)-5 6*indice(1)-4 6*indice(1)-3 ...
+        6*indice(1)-2 6*indice(1)-1 6*indice(1)...
+        6*indice(2)-5 6*indice(2)-4 6*indice(2)-3 ...
+        6*indice(2)-2 6*indice(2)-1 6*indice(2)] ;
+    x1=nodeCoordinates(indice(1),1);
+    y1=nodeCoordinates(indice(1),2);
+    z1=nodeCoordinates(indice(1),3);
+    x2=nodeCoordinates(indice(2),1);
+    y2=nodeCoordinates(indice(2),2);
+    z2=nodeCoordinates(indice(2),3);
+    L = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) +...
+        (z2-z1)*(z2-z1));
+    p = (Iz+Iy)/A;
+    m = rho*A*L/420*[140 0 0 0 0 0 70 0 0 0 0 0;
+                     0  156 0 0 0 22*L 0 54 0 0 0 -13*L;
+                     0 0 156 0 -22*L 0 0 0 54 0 13*L 0;
+                     0 0 0 140*p 0 0 0 0 0 70*p 0 0;
+                     0 0 -22*L 0 4*L^2 0 0 0 -13*L 0 -3*L^2 0;
+                     0 22*L 0 0 0 4*L^2 0 13*L 0 0 0 -3*L^2;
+                     70 0 0 0 0 0 140 0 0 0 0 0;
+                     0 54 0 0 0 13*L 0 156 0 0 0 -22*L;
+                     0 0 54 0 -13*L 0 0 0 156 0 22*L 0;
+                     0 0 0 70*p 0 0 0 0 0 140*p 0 0;
+                     0 0 13*L 0 -3*L^2 0 0 0 22*L 0 4*L^2 0;
+                     0 -13*L 0 0 0 -3*L^2 0 -22*L 0 0 0 4*L^2];
+    if x1 == x2 && y1 == y2
+        if z2 > z1
+            Lambda = [0 0 1 ; 0 1 0 ; -1 0 0];
+        else
+            Lambda = [0 0 -1 ; 0 1 0 ; 1 0 0];
+        end
+    else
+        CXx = (x2-x1)/L;
+        CYx = (y2-y1)/L;
+        CZx = (z2-z1)/L;
+        D = sqrt(CXx*CXx + CYx*CYx);
+        CXy = -CYx/D;
+        CYy = CXx/D;
+        CZy = 0;
+        CXz = -CXx*CZx/D;
+        CYz = -CYx*CZx/D;
+        CZz = D;
+        Lambda = [CXx CYx CZx ;CXy CYy CZy ;CXz CYz CZz];
+    end
+    R = [Lambda zeros(3,9); zeros(3) Lambda zeros(3,6);
+        zeros(3,6) Lambda zeros(3);zeros(3,9) Lambda];
+    mass(elementDof,elementDof)=...
+        mass(elementDof,elementDof)+R'*m*R;
+end
+end
+
+function displacements=solution(GDof,prescribedDof,stiffness,force)
+activeDof=setdiff((1:GDof)', [prescribedDof]);
+U = stiffness(activeDof,activeDof)\force(activeDof);
+displacements = zeros(GDof,1);
+displacements(activeDof)=U;
+end
+
+function [modes,eigenvalues] = eigenvalue(GDof,prescribedDof, ...
+    stiffness,mass,maxEigenvalues)
+activeDof = setdiff((1:GDof)', prescribedDof);
+if maxEigenvalues == 0
+    [V,D] = eig(stiffness(activeDof,activeDof),...
+        mass(activeDof,activeDof));
+else
+    [V,D] = eigs(stiffness(activeDof,activeDof),...
+        mass(activeDof,activeDof),maxEigenvalues,'smallestabs');
+end
+eigenvalues = diag(D);
+modes = zeros(GDof,length(eigenvalues));
+modes(activeDof,:) = V;
+end
